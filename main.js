1006597f1821eaafc9ee7d64ae67b616bb13f1a4
JorgeLG87 <jorge85_6@hotmail.com>
2024-03-17 18:02:10 -0400

    HTML structure, CSS styling and some JavaScript functionality updated

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const name = document.querySelector(".name__input");
+    const author = document.querySelector(".author__input");
+    const imageUrl = document.querySelector(".image__url");
+    const price = document.querySelector(".price__input");
+    
+})
+
