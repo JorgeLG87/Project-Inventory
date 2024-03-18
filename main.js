37b05a0781f30a150b5bb3b508fdc730ceff892a
JorgeLG87 <jorge85_6@hotmail.com>
2024-03-18 10:47:09 -0400

    Added new features to the main.js file

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
 const form = document.querySelector("form");
 const submitButton = document.querySelector(".submit-button");
-submitButton.addEventListener("submit", event => {
+submitButton.addEventListener("click", event => {
     event.preventDefault();
 
-    const name = document.querySelector(".name__input");
-    const author = document.querySelector(".author__input");
-    const imageUrl = document.querySelector(".image__url");
-    const price = document.querySelector(".price__input");
+    // Fetching the data from the form
+
+    const name = document.querySelector(".name__input").value;
+    const author = document.querySelector(".author__input").value;
+    const imageUrl = document.querySelector(".image__url").value;
+    const price = document.querySelector(".price__input").value;
     
+    const mainList = document.querySelector(".main__list");
+
+    // Creating the elements to append the data fetched from the form
+
+    const mainListItem = document.createElement("div");
+    mainListItem.setAttribute("class","main__list__item");
+
+    const mainListSubItem = document.createElement("div");
+    mainListSubItem.setAttribute("class", "main__list__sub-item");
+
+    const imageContainer = document.createElement("div");
+    imageContainer.setAttribute("class", "image-container");
+
+    const image = document.createElement("img");
+    image.setAttribute("class", "image-container__image");
+    image.setAttribute("src", `${imageUrl}`);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.setAttribute("class", "info-container");
+
+    const sublistTitle = document.createElement("h2");
+    sublistTitle.innerText = name;
+
+    const hr = document.createElement("hr");
+    hr.setAttribute("class", "sublist-underline");
+
+    mainList.append(mainListItem);
+    mainListItem.append(mainListSubItem);
+    mainListSubItem.append(imageContainer);
+    imageContainer.append(image);
+
+    mainListSubItem.append(infoContainer);
+    infoContainer.append(sublistTitle);
+    infoContainer.append(hr);
+
+    form.reset();
+})
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", event => {
+    form.reset();
 })
 
