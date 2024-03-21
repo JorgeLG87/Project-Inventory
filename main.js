3f246af2679f0eb451297b960e2d92fcb5e2a243
JorgeLG87 <jorge85_6@hotmail.com>
2024-03-20 22:11:03 -0400

    Updating features

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+// Variables to be used later on
+
+
+
+
 const form = document.querySelector("form");
+
 const submitButton = document.querySelector(".submit-button");
-submitButton.addEventListener("click", event => {
+form.addEventListener("submit", event => {
     event.preventDefault();
 
     // Fetching the data from the form
-
-    const name = document.querySelector(".name__input").value;
-    const author = document.querySelector(".author__input").value;
-    const imageUrl = document.querySelector(".image__url").value;
-    const price = document.querySelector(".price__input").value;
     
+    const name = document.querySelector(".name__input").value;
+    console.log(name);
+
+    const author = document.querySelector(".author__input").value;
+    console.log(author);
+
+    const imageUrl = document.querySelector(".in-stock__dropdown").value;
+    console.log(imageUrl);
+
+    const price = document.querySelector(".price__input").value;
+    console.log(price);
+    
+    const quantityStock = document.querySelector(".quantity-available").value;
+    quantityStock2 = Number(quantityStock);
+
+    let availableTickets = quantityStock2;
+
     const mainList = document.querySelector(".main__list");
 
     // Creating the elements to append the data fetched from the form
@@ -25,16 +43,45 @@ submitButton.addEventListener("click", event => {
 
     const image = document.createElement("img");
     image.setAttribute("class", "image-container__image");
-    image.setAttribute("src", `${imageUrl}`);
+
+    if (imageUrl === "american-airlines") {
+        image.setAttribute("src", "./images/American-Airlines-Logo.png");
+    } else if (imageUrl === "united-airlines") {
+        image.setAttribute("src", "https://www.bcdtravel.com/move-global/wp-content/uploads/sites/142/Move_Global_Spotlight_UnitedAirlines_logo_April2018.png")
+    } else if (imageUrl === "delta") {
+        image.setAttribute("src", "./images/Delta-Logo.jpg");
+    }
 
     const infoContainer = document.createElement("div");
     infoContainer.setAttribute("class", "info-container");
 
+    const departures = document.createElement("div");
+    departures.setAttribute("class", "departures");
+
+    const iconContainer = document.createElement("div");
+    iconContainer.setAttribute("class", "icon-container");
+
+    const icon = document.createElement("img");
+    icon.setAttribute("class", "icon");
+    icon.setAttribute("src", "./images/departures.png");
+
+    const subListContainer = document.createElement("div");
+    subListContainer.setAttribute("class", "sublist-title-container");
+
     const sublistTitle = document.createElement("h2");
+    sublistTitle.setAttribute("class", "sublist-title");
     sublistTitle.innerText = name;
 
     const hr = document.createElement("hr");
     hr.setAttribute("class", "sublist-underline");
+
+    
+
+    const price2 = document.createElement("p");
+    price2.innerText=`$${price}`;
+
+    const availableTick = document.querySelector(".availableTick");
+    availableTick.innerText = `${availableTickets}`;
 
     mainList.append(mainListItem);
     mainListItem.append(mainListSubItem);
@@ -42,9 +89,58 @@ submitButton.addEventListener("click", event => {
     imageContainer.append(image);
 
     mainListSubItem.append(infoContainer);
-    infoContainer.append(sublistTitle);
+    infoContainer.append(departures);
+    departures.append(iconContainer);
+    iconContainer.append(icon);
+    departures.append(subListContainer);
+    subListContainer.append(sublistTitle);
     infoContainer.append(hr);
+    
+    const arrivals = document.createElement("div");
+    arrivals.setAttribute("class", "departures");
 
+    const iconContainer2 = document.createElement("div");
+    iconContainer.setAttribute("class", "icon-container");
+
+    const icon2 = document.createElement("img");
+    icon2.setAttribute("class", "icon2");
+    icon2.setAttribute("src", "./images/arrivals.png");
+
+    const subListContainer2 = document.createElement("div");
+    subListContainer.setAttribute("class", "sublist-title-container");
+
+    const to = document.createElement("h3");
+    to.setAttribute("class", "sublist-author");
+    to.innerText = author;
+
+    infoContainer.append(arrivals);
+    arrivals.append(iconContainer2);
+    iconContainer2.append(icon2);
+    arrivals.append(subListContainer2);
+    subListContainer2.append(to);
+
+
+    infoContainer.append(price2);
+    infoContainer.append(availableTick);
+
+    const inStock = document.createElement("p");
+    inStock.setAttribute("class", "in-stock1");
+    inStock.innerText = "In Stock";
+
+    if (quantityStock2 > 0) {
+        inStock.style.textDecoration = "none";
+    } else {
+        inStock.style.textDecoration = "line-through";
+    }
+
+    infoContainer.append(inStock);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.setAttribute("class", "remove-button");
+    removeBtn.setAttribute("type", "click");
+    removeBtn.innerText = "Quantity Sold";
+
+    infoContainer.append(removeBtn);
     form.reset();
 })
 
@@ -52,4 +148,28 @@ const resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener("click", event => {
     form.reset();
 })
+
+const removeBtn = document.querySelector(".remove-button");
+removeBtn.addEventListener("click", event => {
+    const soldBox = document.querySelector(".sold-box");
+    soldBox.style.display = "flex";
+
+    let availableTick2 = document.querySelector(".availableTick").value;
+
+    const ok = document.querySelector(".sold-btn");
+    ok.addEventListener("click", event => {
+        const currentQuantity = document.querySelector(".availableTick");
+        const quantitySold = document.querySelector(".quantity-sold").value;
+        quantitySold2 = Number(quantitySold);
+
+        let availableTick3 = availableTick2 - quantitySold2;
+
+        currentQuantity.innerText = availableTick3;
+
+        event.target.parentNode.reset();
+        soldBox.style.display = "none";
+        
+    })
+})
+
 
