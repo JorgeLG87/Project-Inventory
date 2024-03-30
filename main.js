df3115d272dd7f1abb8fd86e81d482e4ff64c9bf
JorgeLG87 <jorge85_6@hotmail.com>
2024-03-30 11:03:51 -0400

    New features added

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 // Variables to be used later on
 
-
+let listItems = 0;
+let itemObj = {};
 
 
 const form = document.querySelector("form");
 
 const submitButton = document.querySelector(".submit-button");
+
 form.addEventListener("submit", event => {
     event.preventDefault();
 
+    listItems++;
+    console.log(itemObj);
+
     // Fetching the data from the form
-    
+
     const name = document.querySelector(".name__input").value;
     console.log(name);
 
@@ -24,11 +29,22 @@ form.addEventListener("submit", event => {
     console.log(price);
     
     const quantityStock = document.querySelector(".quantity-available").value;
-    quantityStock2 = Number(quantityStock);
+    let quantityStock2 = Number(quantityStock);
 
     let availableTickets = quantityStock2;
 
+    //localStorage.setItem(JSON.stringify(listItems), JSON.stringify(availableTickets));
+
     const mainList = document.querySelector(".main__list");
+
+
+    // If the inputs of the form are empty
+    const messageBox = document.querySelector(".message-box");
+
+    if (name === "" && author === "") {
+        messageBox.style.display = "flex";
+    }
+
 
     // Creating the elements to append the data fetched from the form
 
@@ -81,66 +97,82 @@ form.addEventListener("submit", event => {
     price2.innerText=`$${price}`;
 
     const availableTick = document.querySelector(".availableTick");
-    availableTick.innerText = `${availableTickets}`;
-
-    mainList.append(mainListItem);
-    mainListItem.append(mainListSubItem);
-    mainListSubItem.append(imageContainer);
-    imageContainer.append(image);
-
-    mainListSubItem.append(infoContainer);
-    infoContainer.append(departures);
-    departures.append(iconContainer);
-    iconContainer.append(icon);
-    departures.append(subListContainer);
-    subListContainer.append(sublistTitle);
-    infoContainer.append(hr);
+    itemObj[listItems] = availableTick;
     
-    const arrivals = document.createElement("div");
-    arrivals.setAttribute("class", "departures");
+    
+    availableTick.innerText = `Available: ${quantityStock2}`;
+    availableTick.setAttribute("id",`${listItems}`);
+    
 
-    const iconContainer2 = document.createElement("div");
-    iconContainer.setAttribute("class", "icon-container");
+    if (name !== "" && author !== "" && price !== "" && quantityStock !== "") {
+        mainList.append(mainListItem);
+        mainListItem.append(mainListSubItem);
+        mainListSubItem.append(imageContainer);
+        imageContainer.append(image);
 
-    const icon2 = document.createElement("img");
-    icon2.setAttribute("class", "icon2");
-    icon2.setAttribute("src", "./images/arrivals.png");
+        mainListSubItem.append(infoContainer);
+        infoContainer.append(departures);
+        departures.append(iconContainer);
+        iconContainer.append(icon);
+        departures.append(subListContainer);
+        subListContainer.append(sublistTitle);
+        infoContainer.append(hr);
+    
+        const arrivals = document.createElement("div");
+        arrivals.setAttribute("class", "departures");
 
-    const subListContainer2 = document.createElement("div");
-    subListContainer.setAttribute("class", "sublist-title-container");
+        const iconContainer2 = document.createElement("div");
+        iconContainer.setAttribute("class", "icon-container");
 
-    const to = document.createElement("h3");
-    to.setAttribute("class", "sublist-author");
-    to.innerText = author;
+        const icon2 = document.createElement("img");
+        icon2.setAttribute("class", "icon2");
+        icon2.setAttribute("src", "./images/arrivals.png");
 
-    infoContainer.append(arrivals);
-    arrivals.append(iconContainer2);
-    iconContainer2.append(icon2);
-    arrivals.append(subListContainer2);
-    subListContainer2.append(to);
+        const subListContainer2 = document.createElement("div");
+        subListContainer.setAttribute("class", "sublist-title-container");
+
+        const to = document.createElement("h3");
+        to.setAttribute("class", "sublist-author");
+        to.innerText = author;
+
+        infoContainer.append(arrivals);
+        arrivals.append(iconContainer2);
+        iconContainer2.append(icon2);
+        arrivals.append(subListContainer2);
+        subListContainer2.append(to);
 
 
-    infoContainer.append(price2);
-    infoContainer.append(availableTick);
+        infoContainer.append(price2);
 
-    const inStock = document.createElement("p");
-    inStock.setAttribute("class", "in-stock1");
-    inStock.innerText = "In Stock";
+    
+        infoContainer.append(availableTick);
 
-    if (quantityStock2 > 0) {
-        inStock.style.textDecoration = "none";
-    } else {
-        inStock.style.textDecoration = "line-through";
+        const inStock = document.createElement("p");
+        inStock.setAttribute("class", "in-stock1");
+        inStock.innerText = "In Stock";
+
+        if (quantityStock !== "0") {
+            inStock.style.textDecoration = "none";
+        } else {
+            inStock.style.textDecoration = "line-through";
+        }
+
+        infoContainer.append(inStock);
+
+        const removeBtn = document.createElement("button");
+        removeBtn.setAttribute("class", "remove-button");
+        removeBtn.setAttribute("type", "button");
+        removeBtn.innerText = "Quantity Sold";
+
+        infoContainer.append(removeBtn);
+
+        const removeCompleteBtn = document.createElement("button");
+        removeCompleteBtn.setAttribute("class", "completely-remove");
+        removeCompleteBtn.setAttribute("type", "button");
+        removeCompleteBtn.innerText = "Remove";
+
+        infoContainer.append(removeCompleteBtn);
     }
-
-    infoContainer.append(inStock);
-
-    const removeBtn = document.createElement("button");
-    removeBtn.setAttribute("class", "remove-button");
-    removeBtn.setAttribute("type", "click");
-    removeBtn.innerText = "Quantity Sold";
-
-    infoContainer.append(removeBtn);
     form.reset();
 })
 
@@ -149,27 +181,37 @@ resetButton.addEventListener("click", event => {
     form.reset();
 })
 
-const removeBtn = document.querySelector(".remove-button");
-removeBtn.addEventListener("click", event => {
-    const soldBox = document.querySelector(".sold-box");
-    soldBox.style.display = "flex";
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", event => {
+    const messageBox = document.querySelector(".message-box");
 
-    let availableTick2 = document.querySelector(".availableTick").value;
-
-    const ok = document.querySelector(".sold-btn");
-    ok.addEventListener("click", event => {
-        const currentQuantity = document.querySelector(".availableTick");
-        const quantitySold = document.querySelector(".quantity-sold").value;
-        quantitySold2 = Number(quantitySold);
-
-        let availableTick3 = availableTick2 - quantitySold2;
-
-        currentQuantity.innerText = availableTick3;
-
-        event.target.parentNode.reset();
-        soldBox.style.display = "none";
-        
-    })
+    messageBox.style.display = "none";
 })
+
+const listOfFlights = document.querySelectorAll(".main__list");
+console.log(listOfFlights);
+
+for (let i = 0; i < listOfFlights.length; i++) {
+    const removeBtn = document.querySelector(".remove-button");
+    //const available = localStorage.getItem(JSON.parse(i));
+    //console.log(available);
+
+
+
+    removeBtn.addEventListener("click", event => {
+        const messageBox = document.querySelector(".sold-box");
+        messageBox.style.display = "flex";
+
+        const soldBtn = document.querySelector(".sold-btn");
+        soldBtn.addEventListener("click", event => {
+            messageBox.style.display = "none";
+        })
+    })
+
+    const removeCompleteBtn = document.querySelector(".completely-remove");
+    removeCompleteBtn.addEventListener("click", event => {
+        event.target.parentNode.parentNode.remove();
+    })
+}
 
 
